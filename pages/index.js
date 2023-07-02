2e981cd9321153ca4c86f5abb79ddd21185946fb
josedavidnup <josedavidnup@gmail.com>
2023-07-01 20:01:30 -0400

    new version with next.js and tailwind

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>
+          Jose David | Full Stack Developer | Machine Learning Developer
+        </title>
+        <meta
+          name='description'
+          content='Empowering digital innovation through a fusion of Full Stack and Machine Learning expertise. Elevate your projects with seamless web development and cutting-edge machine learning solutions. Unleash the potential of technology with our versatile development skills.'
+        />
+        <link rel='icon' href='/jd_logo.png' />
+      </Head>
+    </div>
+  );
+}
