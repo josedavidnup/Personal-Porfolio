6f7a6f257566c341e3350eae5575060af5dba142
josedavidnup <josedavidnup@gmail.com>
2023-07-02 22:19:21 -0400

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         />
         <link rel='icon' href='/jd_logo.png' />
       </Head>
+      <Navbar />
     </div>
   );
 }
